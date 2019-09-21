b63b3126cb1e21295e8273bf7ee2a76bd53bdde6
RockVII <40158793+RockVII@users.noreply.github.com>
2019-09-21 14:23:58 -0600

    active.js

---
+++ AgenciaVuelos/public/js/active.js
@@ -0,0 +1,19 @@
+let url = window.location
+let menu = document.querySelector('#menu')
+menu = menu.getElementsByTagName('li')
+
+if(url == 'http://127.0.0.1:8000/clasetarifa'){
+	menu[2].className += ' active'
+}
+else if(url == 'http://127.0.0.1:8000/rutas'){
+	menu[3].className += ' active'
+}
+else if(url == 'http://127.0.0.1:8000/aviones'){
+	menu[4].className += ' active'
+}
+else if(url == 'http://127.0.0.1:8000/viajes'){
+	menu[5].className += ' active'
+}
+else{
+	menu[1].className += ' active'
+}
--- AgenciaVuelos/resources/views/plantilla.blade.php
+++ AgenciaVuelos/resources/views/plantilla.blade.php
@@ -3,6 +3,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 	<title>@yield('titulo')</title>
+
 	<link href="https://fonts.googleapis.com/css?family=Barlow+Condensed:400,700,900|Merriweather:400,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link href="{{ asset('css/adminStyle.css') }}" rel="stylesheet">
@@ -11,7 +12,6 @@
 <body id='body'>
 	<nav id="menu" class="navbar navbar-expand-lg">
 		<li class="navbar-brand logo"><a href="">ABBIANKKA</a></li>
-		<li class="nav-item"><a class="nav-link" href="http://127.0.0.1:8000/clasetarifa"><span class="icon"><img src="img/home.svg" alt=""></span>Inicio</a></li>
 		<li class="nav-item"><a class="nav-link" href="http://127.0.0.1:8000/clasetarifa"><span class="icon"><img src="img/home.svg" alt=""></span>Inicio</a></li>
 		<li class="nav-item"><a class="nav-link" href="http://127.0.0.1:8000/clasetarifa"><span class="icon"><img src="img/seat.svg" alt=""></span>Clases/Tarifas</a></li>
 		<li class="nav-item"><a class="nav-link" href="http://127.0.0.1:8000/rutas"><span class="icon"><img src="img/route.svg" alt=""></span>Ciudades/Rutas</a></li>
@@ -31,28 +31,8 @@
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<script src="js/js.js"></script>
-	<script type="text/javascript">
-		window.onLoad = getURL()
-		function getURL(){
-			let url = window.location
-			let menu = document.querySelector('#menu')
-			menu = menu.getElementsByTagName('li')
+	{{-- <script src="js/js.js"></script> --}}
+	<script src="js/active.js"></script>
 
-			if(url == 'http://127.0.0.1:8000/clasetarifa'){
-				menu[2].className += ' active'
-			}
-			if(url == 'http://127.0.0.1:8000/rutas'){
-				menu[3].className += ' active'
-			}
-			if(url == 'http://127.0.0.1:8000/aviones'){
-				menu[4].className += ' active'
-			}
-			if(url == 'http://127.0.0.1:8000/viajes'){
-				menu[5].className += ' active'
-			}
-
-		}
-	</script>
 </body>
 </html>
