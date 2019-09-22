c16bb39eb9545a24673781dc66e9d01c96c15c37
IvonneSoriano <ivonnesoriano25@gmail.com>
2019-09-22 13:06:21 -0600

    Fixed the width of the email input

--- AgenciaVuelos/resources/views/auth/login.blade.php
+++ AgenciaVuelos/resources/views/auth/login.blade.php
@@ -12,9 +12,9 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Usuario') }}</label>
+                            <label for="email" class="col-md-3 col-form-label text-md-right">{{ __('Usuario') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-9">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -28,7 +28,7 @@
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
