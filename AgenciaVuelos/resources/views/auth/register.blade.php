621d52b1dd04185f49d5df0a528ab8fffbf396bc
IvonneSoriano <ivonnesoriano25@gmail.com>
2019-10-07 17:06:34 -0600

    change the button in the index

--- AgenciaVuelos/resources/views/auth/register.blade.php
+++ AgenciaVuelos/resources/views/auth/register.blade.php
@@ -12,10 +12,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-3 col-form-label text-md-right">{{ __('Nombre') }}</label>
+                            <!-- <label for="name" class="col-md-3 col-form-label text-md-right">{{ __('Nombre') }}</label> -->
 
-                            <div class="col-md-9">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="nombre">
+                            <div class="col-md-12">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Nombre completo">
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -26,9 +26,9 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-3 col-form-label text-md-right">{{ __('Correo') }}</label>
+                            <!-- <label for="email" class="col-md-3 col-form-label text-md-right">{{ __('Correo') }}</label> -->
 
-                            <div class="col-md-9">
+                            <div class="col-md-12">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="user@example.com">
 
                                 @error('email')
@@ -40,10 +40,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <!-- <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label> -->
 
-                            <div class="col-md-8">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                            <div class="col-md-12">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Contraseña">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -54,18 +54,18 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Password') }}</label>
+                            <!-- <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirmar Password') }}</label> -->
 
-                            <div class="col-md-8">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            <div class="col-md-12">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Confirmar contraseña">
                             </div>
                         </div>
 
                         <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">Tarjeta</label>
+                                <!-- <label for="name" class="col-md-4 col-form-label text-md-right">Tarjeta</label> -->
     
-                                <div class="col-md-6">
-                                    <input id="tarjeta" type="text" class="form-control @error('tarjeta') is-invalid @enderror" name="tarjeta" value="{{ old('tarjeta') }}" required autocomplete="tarjeta" autofocus maxlength="25">
+                                <div class="col-md-12">
+                                    <input id="tarjeta" type="text" class="form-control @error('tarjeta') is-invalid @enderror" name="tarjeta" value="{{ old('tarjeta') }}" required autocomplete="tarjeta" autofocus maxlength="25" placeholder="Tarjeta de debito o credito">
     
                                     @error('tarjeta')
                                         <span class="invalid-feedback" role="alert">
--- AgenciaVuelos/resources/views/welcome.blade.php
+++ AgenciaVuelos/resources/views/welcome.blade.php
@@ -60,27 +60,54 @@
                             @csrf
                         </form>
                     </li>
-                @else                
-                    <li>
-                        <a href="{{ route('login') }}"><span>
-                                <svg class="menu-logo" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                                    viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
-                                    <g>
-                                        <g>
-                                            <path d="M437.02,330.98c-27.883-27.882-61.071-48.523-97.281-61.018C378.521,243.251,404,198.548,404,148
-                                                C404,66.393,337.607,0,256,0S108,66.393,108,148c0,50.548,25.479,95.251,64.262,121.962
-                                                c-36.21,12.495-69.398,33.136-97.281,61.018C26.629,379.333,0,443.62,0,512h40c0-119.103,96.897-216,216-216s216,96.897,216,216
-                                                h40C512,443.62,485.371,379.333,437.02,330.98z M256,256c-59.551,0-108-48.448-108-108S196.449,40,256,40
-                                                c59.551,0,108,48.448,108,108S315.551,256,256,256z"/>
-                                        </g>
-                                    </g>
-                                </svg>
-                        </span></a>
-                    </li>
-                    @if (Route::has('register'))
-                        <li><a href="{{ route('register') }}">Register</a></li>
+                @else     
+                <li>
+                <a class="" href="{{ route('login') }}">Login</a>
+                </li>
+    @if (Route::has('register'))
+    <li>
+    <a class="" href="{{ route('register') }}">Registrarse</a></li>
+                    @endif           
+<!-- 
+                <li class="dropdown">
+  <button class="btn btn-gold dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Usuario
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="{{ route('login') }}">Login</a>
+    @if (Route::has('register'))
+    <a class="dropdown-item" href="{{ route('register') }}">Registrarse</a>
                     @endif
+    
+  </div>
+</li> -->
+                   
                 @endauth
+                <div class="dropdown">
+  <button class="btn btn-gold dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  @auth
+                     {{ Auth::user()->name }}
+                    @else
+                    Usuario
+                @endauth
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+  @auth
+  <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                            @csrf
+                        </form>
+                        @else
+  <a class="dropdown-item" href="{{ route('login') }}">Login</a>
+  @if (Route::has('register'))
+  <a class="dropdown-item" href="{{ route('register') }}">Registrarse</a>
+  @endif
+  @endauth
+  </div>
+</div>
             </nav>
         </div>
     </header>
@@ -463,6 +490,9 @@
 
     <script src="js/jquery-3.4.1.min.js"></script>
     <script src="js/slick.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
     <script src="js/wow.js"></script>
     <script src="js/jquery.waypoints.min.js"></script>
     <script src="js/jquery.counterup.min.js"></script>
